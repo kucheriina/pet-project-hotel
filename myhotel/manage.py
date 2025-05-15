a9b4a7d8fb3a302398ee1bf0cae61b3085780440
annakucerina <kucheriina1075@gmail.com>
2025-05-15 20:20:24 +0400

    chore(env): load .env file in manage.py via python-dotenv

--- myhotel/manage.py
+++ myhotel/manage.py
@@ -2,6 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 def main():
